8740c10a8785c2fa9e6fbc27055160ddbca73705
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-04-21 14:32:17 +0530

    8

--- t.py
+++
@@ -1,8 +0,0 @@
-import markdown
-
-markdown_content = '[Artificial general intelligence (AGI)](https://en.wikipedia.org/wiki/Artificial_general_intelligence)'
-
-html_content = markdown.markdown(markdown_content)
-
-# Print or use the HTML content
-print(html_content)
