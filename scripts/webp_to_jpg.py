549543bddfa349488dc708a5ae01ba6f156ff434
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-10-08 09:47:48 +0530

    45

--- scripts/webp_to_jpg.py
+++ scripts/webp_to_jpg.py
@@ -1,9 +1,13 @@
 from PIL import Image
 
-file_name = r"static\imgs\1"
+file_name = r"static\imgs\45"
 
 # Open the WEBP image
 webp_image = Image.open(f"{file_name}.webp")
 
 # Convert and save as JPG
 webp_image.convert("RGB").save(f"{file_name}.jpeg", "JPEG")
+
+# delete the WEBP image
+import os
+os.remove(f"{file_name}.webp")
